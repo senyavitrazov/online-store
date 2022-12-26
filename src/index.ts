d32b60311e9aad28dfe5ec9fcee9e0dd959d40ce
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2022-12-26 15:56:30 +0300

    feat:add checkbox filter prototype

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { DualSlider } from './ts/dual-slider';
 import { ProductCard } from './ts/product-card';
-
+import { Checkbox } from './ts/checkbox-filter';
 window.customElements.define('dual-slider', DualSlider);
 window.customElements.define('product-card', ProductCard);
+const checkbox = new Checkbox();
+checkbox.drawcheckboxCategories();
+checkbox.drawcheckboxBrand();
