3daad9544d19f1ed02ef11d4a8595e41903a9b8f
vitrazov2 <arsenii2509@gmail.com>
2022-12-29 22:38:06 +0300

    feat: add search block

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,11 @@ checkbox.drawcheckboxCategories();
 checkbox.drawcheckboxBrand();
 
 class App {
-  productList = document.querySelector('.product-list');
   productsData: Product[] = dataExample.products;
   body = document.querySelector('body');
   main = document.querySelector('main');
+  productsBlock = document.querySelector('.products');
+  productList = document.querySelector('.product-list');
 
   constructor() {
     window.customElements.define('dual-slider', DualSlider);
@@ -22,15 +23,27 @@ class App {
     if (!this.main) {
       this.body && this.body.insertAdjacentHTML('afterend', '<main><div class="main__wrapper wrapper"></div></main>');
     }
+
+    const mainWrapper = this.main?.querySelector('.main__wrapper');
+
+    if (!this.productsBlock) {
+      this.productsBlock = document.createElement('div');
+      this.productsBlock.classList.add('products');
+      this.productsBlock.innerHTML =
+        '<div class="products__top"><div class="products__search search"><button class="search__button"></button><input type="text" ' +
+        'name="search" id="search-input" placeholder="search product"></div><sort-select></sort-select></div><div class="products__bottom product-list"></div>';
+      mainWrapper?.append(this.productsBlock);
+    }
+
     //filters block
     if (!this.productList) {
       this.productList = document.createElement('div');
-      this.productList.classList.add('.product-list');
+      this.productList.classList.add('product-list');
+      this.productsBlock?.append(this.productList);
     }
-    // let i = this.productsData.length;
-    let i = 6;
-    while (i) {
-      i--;
+    let i = 0;
+    while (i <= this.productsData.length) {
+      i++;
       this.productList.innerHTML += `<product-card src="${i}"></product-card>`;
     }
   }
